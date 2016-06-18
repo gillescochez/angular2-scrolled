c5710a6ffe077fb489539f91296fae1bba61cab8
Gilles Cochez <gillescochez@gmail.com>
2016-06-18 17:33:06 +0100

    Port all features from angular-scrolled

--- angular2-scrolled.js
+++ angular2-scrolled.js
@@ -1,4 +1,5 @@
-import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, Output, OnInit, OnDestroy, EventEmitter } from "@angular/core";
+import { Observable, Subscription } from "rxjs/Rx";
 
 @Directive({
     selector: '[scrolled]'
@@ -6,25 +7,75 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 export class ScrolledDirective {
 
+    direction: String = "vertical";
+    threshold: Number = 0;
+    enabled: Boolean = true;
+
+    container: Object;
+    subscription: Subscription;
+    element: HTMLElement;
+    canTrigger: Function;
+
+    @Input("scrolledWindow") useWindow: Boolean = false;
+
+    @Output() onScrolled = new EventEmitter();
+
     constructor(el: ElementRef) {
-        this.el = el.nativeElement;
-        console.log(this.el);
+        this.element = el.nativeElement;
     }
 
-    @Input() set scrolledDirection(direction) {
-        console.log("direction", direction);
+    ngOnInit() {
+
+        this.container = this.useWindow ? window : this.element;
+        this.canTrigger = this.useWindow ? function() {
+
+            var doc = document.documentElement;
+
+            if (this.direction === "vertical") {
+                return this.enabled && ((doc.offsetHeight - window.innerHeight) - (window.pageYOffset - (doc.clientTop || 0)) <= this.threshold);
+            } else {
+                return this.enabled && ((doc.offsetWidth - window.innerWidth) - (window.pageXOffset - (doc.clientLeft || 0)) <= this.threshold);
+            }
+
+        } : function() {
+
+            var raw = this.element;
+
+            if (this.direction === "vertical") {
+                return this.enabled && (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight - this.threshold);
+            } else {
+                return this.enabled && (raw.scrollLeft + raw.offsetWidth >= raw.scrollWidth - this.threshold);
+            }
+        };
+
+        this.subscribe();
     }
 
-    @Input() set scrolledThreshold(threshold) {
-        console.log("threshold", threshold);
+    subscribe() {
+        this.subscription = Observable.fromEvent(this.container, "scroll").subscribe(this.handler.bind(this));
     }
 
-    @Input() set scrolledEnabled(enabled) {
-        console.log("enabled", enabled);
+    handler() {
+        if (this.canTrigger()) {
+            this.onScrolled.next({});
+        }
     }
 
-    @Input() set scrolledWindow(window) {
-        console.log("window", window);
+    ngOnDestroy() {
+        if (this.subscription) {
+            this.subscription.unsubscribe();
+        }
     }
 
+    @Input() set scrolledDirection(value: String) {
+        this.direction = value;
+    }
+
+    @Input() set scrolledThreshold(value: Number) {
+        this.threshold = value;
+    }
+
+    @Input() set scrolledEnabled(value: Boolean) {
+        this.enabled = value;
+    }
 }
