a10aa3afdc06526c45afd4b081ee7fc35cdf43bc
Gilles Cochez <gillescochez@gmail.com>
2016-06-18 17:34:02 +0100

    Support all examples

--- demo/demo.js
+++ demo/demo.js
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { DomSanitizationService } from "@angular/platform-browser";
 import { bootstrap } from "@angular/platform-browser-dynamic";
 import { ScrolledDirective } from "../angular2-scrolled";
 
@@ -10,17 +11,41 @@ import { ScrolledDirective } from "../angular2-scrolled";
 
 export class AppComponent {
 
-    enabled = true;
-    items = [];
+    enabled: Boolean = true;
+    items: Array = [];
+    hItems: Array = [];
+    windowItems: Array = [];
+    hWidth: String;
+    trustStyle: Function;
 
-    constructor() {
+    constructor(sanitizer: DomSanitizationService) {
+        this.trustStyle = sanitizer.bypassSecurityTrustStyle;
         this.more();
+        this.hMore();
+        this.windowMore();
     }
 
     more() {
         (new Array(20)).fill(0).forEach(() => {
             this.items.push({
                 name: this.items.length + 1
+            });
+        });
+    }
+
+    hMore() {
+        (new Array(20)).fill(0).forEach(() => {
+            this.hItems.push({
+                name: this.hItems.length + 1
+            });
+        });
+        this.hWidth = this.trustStyle((50 * this.hItems.length) + "px");
+    }
+
+    windowMore() {
+        (new Array(20)).fill(0).forEach(() => {
+            this.windowItems.push({
+                name: this.windowItems.length + 1
             });
         });
     }
